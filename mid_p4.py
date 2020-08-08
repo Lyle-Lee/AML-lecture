0f0c38e85ff5950c0d00e56b77c569217a13ced5
Lyle Lee <lyle@Lyles-MacBook-Pro.local>
2020-08-08 23:11:40 +0900

    update source code

--- mid_p4.py
+++ mid_p4.py
@@ -1,16 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from svm import SVC
+import cvxpy as cv
+#from svm import SVC
 
 np.random.seed(630)
 n = 200
+features = 4
+noise = 0.5 * np.random.randn(n)
 lam = 0.01
-x = 3 * (np.random.rand(n, 4) - 0.5)
-y = (2 * x[:, 0] - 1 * x[:,1] + 0.5 + 0.5 * np.random.randn(n)) > 0
-y = 2 * y -1
+x = 3 * (np.random.randn(n, 4) - 0.5)
+y = 2 * (2 * x[:, 0] - x[:, 1] + 0.5 + noise > 0) - 1
+#x = np.matrix(np.random.normal(size=n * features).reshape(n, features))  # gausian distributed
+#y = 2 * (x.sum(axis=1) > 0) - 1.0
 
-clf = SVC(kernel="linear", C=1.0)
-clf.fit(x, y)
+# experiment with svm API
+#clf = SVC(kernel="linear", C=1.0)
+#clf.fit(x, y)
+#pred = clf.predict(np.array([-2.76 ,-3.05]).reshape(1, 2))
+#print(pred)
+
+# cvx
+w_lasso = cv.Variable((4,1))
+obj_fn = cv.sum(cv.pos(1 - cv.multiply(y.reshape(200, 1), x @ w_lasso))) +  lam * cv.norm(w_lasso, 1)
+objective = cv.Minimize(obj_fn)
+#constraints = []
+prob = cv.Problem(objective)
+result = prob.solve(solver=cv.CVXOPT) 
+w_lasso = w_lasso.value
 
 # batch proximal subgradient
 num_iter = 1500
@@ -24,23 +40,21 @@ for t in range(num_iter):
     reg = lam * np.sum(np.abs(w))
     ywx = y * np.dot(x, w)
     hinge_loss = np.sum(np.maximum(np.zeros(n), 1 - ywx)) + reg
+    grad = np.zeros(4)
+    for i in range(n):
+        if ywx[i] < 1:
+            grad += -(y * x.T).T[i]
+        elif ywx[i] == 1:
+            grad += -0.5 * (y * x.T).T[i]
+    g = np.where(w > 0, 1, w)
+    g = np.where(g < 0, -1, g)
+    subgrad = grad + np.where(g == 0, 0, g)
     w_history.append(w)
     loss_history.append(hinge_loss)
-    grad = np.zeros(4)
-    for i in range(len(ywx)):
-        if ywx[i] < 1:
-            grad += -1
-        elif ywx[i] == 1:
-            grad += -0.5
-    subgrad = np.where(w >= 0, 1, -1)
-    w += alpha * (grad + subgrad)
+    w = w + alpha * (-subgrad)
 
-#plt.plot(w_history, hinge_loss, label='hinge loss')
-#plt.legend()
-#plt.xlim(-2,2)
-#plt.ylim(0,2)
-#plt.xlabel('w')
-#plt.ylabel('loss')
-print(hinge_loss)
-pred = clf.predict(np.array([-2.76 ,-3.05]).reshape(1, 2))
-print(pred)
+plt.plot(np.abs(w_history - w_lasso.reshape(-1)), label='hinge loss')
+plt.legend()
+plt.xlabel('iteration')
+plt.ylabel('|w_prox - w_cvx|')
+plt.show()
